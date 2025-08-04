cefdc141a7292af75a95b2b153e13dafc198640b
Márcio Santos <129344662+MarcioSVS96@users.noreply.github.com>
2025-08-04 11:00:40 -0300

    Revert "first commit"
    This reverts commit e06a6c65e4ffcec69e8af30d8b6dd52cd80f6413.

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  testEnvironment: "node",
-  collectCoverageFrom: ["src/**/*.js", "!src/server.js", "!src/config/passport.js"],
-  coverageDirectory: "coverage",
-  coverageReporters: ["text", "lcov", "html"],
-  testMatch: ["**/__tests__/**/*.test.js"],
-  setupFilesAfterEnv: ["<rootDir>/__tests__/setup.js"],
-  testTimeout: 10000,
-}
